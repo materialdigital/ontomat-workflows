51d2257763021b186f8dac27b7ef579d3890b71d
Nicolas Christ <nicolas.christ@iwm.fraunhofer.de>
2025-09-17 13:14:40 +0200

    Update WF

--- Scripts/abaqus_runner.py
+++ Scripts/abaqus_runner.py
@@ -10,10 +10,9 @@ import subprocess
 
 def abaqus_runner(job_path, job_name, abaqus_cores):
 
-
-    call_command = "cd {job_path}; abawrapper -v 2023 -j {job_name} -cpus {abaqus_cores} -l lic4 -inter".format(
+    call_command = "cd {job_path}; abawrapper -v 2023 -j {job_name} -cpus {abaqus_cores} -l lic5 -inter".format(
         job_path=job_path, job_name=job_name, abaqus_cores=abaqus_cores)
-        
+
     subprocess.call(call_command, shell=True)
 
     print(call_command)
--- Scripts/extract_homogenized_stiffness.py
+++ Scripts/extract_homogenized_stiffness.py
@@ -3,41 +3,55 @@ from abaqusConstants import *
 from odbMaterial import *
 from odbSection import *
 
+import sys
 import numpy as np
 
-# odb_path = "W:\OntOMat\periodic\UD\Job-1.odb"
-odb_path = r"Periodic_Homogenization_UD.odb"
-vol = 45.2**3
+def homogenize(sim_path, job_name, RVE_volume):
 
-odb = openOdb(path=odb_path)
+    vol = RVE_volume
+    
+    odb_path = sim_path + job_name + '.odb'
 
-step = odb.steps['HomogStiff-Initial']
+    odb = openOdb(path=odb_path)
 
-region_keys = step.historyRegions.keys()
+    step = odb.steps['HomogStiff-Initial']
 
-region_node_normal = step.historyRegions[region_keys[0]]
-region_node_shear = step.historyRegions[region_keys[1]]
+    region_keys = step.historyRegions.keys()
 
-generic_string = 'RF{} Load case LC-{}-HOMOGSTIFF-INITIAL-E{}'
+    region_node_normal = step.historyRegions[region_keys[0]]
+    region_node_shear = step.historyRegions[region_keys[1]]
 
-homogenized_stiffness_voigt = np.zeros((6,6)) # non-normalized Voigt notation
+    generic_string = 'RF{} Load case LC-{}-HOMOGSTIFF-INITIAL-E{}'
 
-RF_sequence = [1,2,3,1,2,3]
-E_sequence = [11,22,33,12,13,23]
+    homogenized_stiffness_voigt = np.zeros((6,6)) # non-normalized Voigt notation
 
-for i in range(6):
-    for j in range(6):
-        specific_string = generic_string.format(RF_sequence[j], int(i+1), E_sequence[i])
-        if j < 3:
-            stiffness = region_node_normal.historyOutputs[specific_string].data[0][1]/vol
-        else:
-            stiffness = region_node_shear.historyOutputs[specific_string].data[0][1]/(2*vol)
-            
-        homogenized_stiffness_voigt[i,j] = stiffness
+    RF_sequence = [1,2,3,1,2,3]
+    E_sequence = [11,22,33,12,13,23]
 
-homogenized_stiffness_mandel = homogenized_stiffness_voigt.copy() # normalized Mandel notation
-homogenized_stiffness_mandel[:,3:] *= np.sqrt(2)
-homogenized_stiffness_mandel[3:,:] *= np.sqrt(2)
+    for i in range(6):
+        for j in range(6):
+            specific_string = generic_string.format(RF_sequence[j], int(i+1), E_sequence[i])
+            if j < 3:
+                stiffness = region_node_normal.historyOutputs[specific_string].data[0][1]/vol
+            else:
+                stiffness = region_node_shear.historyOutputs[specific_string].data[0][1]/(2*vol)
+                
+            homogenized_stiffness_voigt[i,j] = stiffness
 
-np.savetxt("homogenized_stiffness_voigt.csv", homogenized_stiffness_voigt, delimiter=",")
-np.savetxt("homogenized_stiffness_mandel.csv", homogenized_stiffness_mandel, delimiter=",")
+    homogenized_stiffness_mandel = homogenized_stiffness_voigt.copy() # normalized Mandel notation
+    homogenized_stiffness_mandel[:,3:] *= np.sqrt(2)
+    homogenized_stiffness_mandel[3:,:] *= np.sqrt(2)
+
+    np.savetxt(sim_path + "homogenized_stiffness_voigt.csv", homogenized_stiffness_voigt, delimiter=",")
+    np.savetxt(sim_path + "homogenized_stiffness_mandel.csv", homogenized_stiffness_mandel, delimiter=",")
+
+
+if __name__ == "__main__":
+    
+    input_args = sys.argv
+ 
+    RVE_volume = float(input_args[-1])
+    sim_path = str(input_args[-3])
+    job_name = str(input_args[-2])
+    
+    homogenize(sim_path, job_name, RVE_volume)
